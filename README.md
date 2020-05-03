6820317a0f473fbd7290833f709792a82fbc0d42
Selen <tabletevko@gmail.com>
2020-05-03 15:16:12 +0200

    My First Web Project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Calculator
+
+This is my first web project.  
+It's a calculator.
+
+## How to set up Git (tips)
+
+Download Git for Windows  
+https://gitforwindows.org/
+
+Generating a new SSH key and adding it to the ssh-agent  
+https://help.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+
+Windows 10 Credentials issue
+https://stackoverflow.com/a/54958975/1331443
---
+++ index.js
@@ -0,0 +1,52 @@
+// Functions
+function get(name) {
+  return document.querySelector('[name="' + name + '"]');
+}
+
+// Plus
+plus1 = get("plus1");
+plus2 = get("plus2");
+plusResult = document.getElementById("plusResult");
+
+plus1.addEventListener("input", plusValue);
+plus2.addEventListener("input", plusValue);
+
+function plusValue(e) {
+  plusResult.textContent = Number(plus1.value) + Number(plus2.value);
+}
+
+// Minus
+minus1 = get("minus1");
+minus2 = get("minus2");
+minusResult = document.getElementById("minusResult");
+
+minus1.addEventListener("input", minusValue);
+minus2.addEventListener("input", minusValue);
+
+function minusValue(e) {
+  minusResult.textContent = Number(minus1.value) - Number(minus2.value);
+}
+
+// times
+times1 = get("times1");
+times2 = get("times2");
+timesResult = document.getElementById("timesResult");
+
+times1.addEventListener("input", timesValue);
+times2.addEventListener("input", timesValue);
+
+function timesValue(e) {
+  timesResult.textContent = Number(times1.value) * Number(times2.value);
+}
+
+// split
+split1 = get("split1");
+split2 = get("split2");
+splitResult = document.getElementById("splitResult");
+
+split1.addEventListener("input", splitValue);
+split2.addEventListener("input", splitValue);
+
+function splitValue(e) {
+  splitResult.textContent = Number(split1.value) / Number(split2.value);
+}
